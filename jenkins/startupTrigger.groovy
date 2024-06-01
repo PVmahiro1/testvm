f66ea4ea2414d2f71f213a5a99e409d15b69d3a8
PVmahiro1 <164232339+PVmahiro1@users.noreply.github.com>
2024-06-01 11:41:32 +0000

    test

--- jenkins/startupTrigger.groovy
+++ jenkins/startupTrigger.groovy
@@ -1,7 +1,7 @@
 import jenkins.model.*
 
 def instance = Jenkins.getInstance()
-def job = instance.getItem('Lab_5')
+def job = instance.getItem('Lab-5')
 
 if (job) {
     println("Triggering job: ${job.name}")
